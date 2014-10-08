f6fd34d6ae71a1466ff5316609d6eec581b68df9
Thomas Peng <debianthomas@gmail.com>
2014-10-09 03:30:40 +0800

    finish the basic functions and add the Chinese version

--- src/main/webapp/resources/js/demo.js
+++ src/main/webapp/resources/js/demo.js
@@ -4,22 +4,109 @@
 (function(){
     var demo = {
         config: {
+            protocol: 'http',
             ip: '180.168.36.241',
             port: '',
-            root: '/p2p',
-            apiPath: '/api'
+            apiPath: 'api'
+        },
+        state: {
+            version: '0.9.1',
+            lastModified: 'Oct 9 2014',
+            historyUpToDateFront: false,
+            historyUpToDateBack: false
         },
         model: {
-            recognizeFront: function(file) {},
-            recognizeBack: function(file) {},
-            getHistoryFront: function(callback) {},
+            recognizeIdCard: function(file, frontOrBack, callback, errorHandler) {
+                var formData = new FormData();
+                formData.append("file", file);
+                frontOrBack = frontOrBack.charAt(0).toUpperCase() + frontOrBack.substr(1).toLowerCase();
+                $.ajax({
+                    url: demo.config.apiPath + '/idcard/' + frontOrBack,
+                    type: 'POST',
+                    data: formData,
+                    cache: false,
+                    processData: false,
+                    contentType: false,
+                    dataType: 'json',
+                    success: callback,
+                    error: errorHandler
+                });
+            },
+            readAsUrl: function(file, callback) {
+                var reader = new FileReader();
+                reader.onload = function(e) {
+                    var url = e.target.result;
+                    callback(url);
+                };
+                reader.readAsDataURL(file);
+            },
+            getHistoryFront: function(callback) {
+                $.get(demo.config.apiPath + '/idcard/front', callback, 'json');
+            },
             getHistoryBack: function(callback) {}
         },
         view: {
-            showRecognitionResultFront: function() {},
-            showRecognitionResultBack: function() {},
-            showHistoryFront: function() {},
-            showHistoryBack: function() {}
+            showUrlImgFront: function(url) {
+                $('#img-front').attr('src', url);
+            },
+            showRecognitionResultFront: function(json) {
+                $('#name').html(json.name);
+                $('#cardNo').html(json.cardNo);
+                $('#gender').html(json.sex);
+                $('#birthday').html(json.birthday);
+                $('#address').html(json.address);
+            },
+            showUrlImgBack: function(url) {
+                $('#img-back').attr('src', url);
+            },
+            showRecognitionResultBack: function(json) {
+                $('#validPeriod').html(json.validPeriod);
+                $('#releasedBy').html(json.issueAuthority);
+            },
+            showHistoryFront: function(json) {
+                var template = '<td class="history-file-front"><a data-toggle="modal" data-target="#img-box">IMAGE</a></td>' +
+                    '<td class="history-name"></td>' +
+                    '<td class="history-cardNo"></td>' +
+                    '<td class="history-gender"></td>' +
+                    '<td class="history-address"></td>' +
+                    '<td class="recognized-at-front"></td>',
+                    tbody = $('<tbody></tbody>'),
+                    tr;
+                for(var i = 0, l= json.length; i < l; i++) {
+                    tr = $('<tr></tr>').html(template);
+                    tr.find('td[class="history-file-front"] a').attr('data-url', demo.config.apiPath + '/resources/' + json[i].source + '.jpg');
+                    tr.find('td[class="history-name"]').html(json[i].name);
+                    tr.find('td[class="history-cardNo"]').html(json[i].cardNo);
+                    tr.find('td[class="history-gender"]').html(json[i].sex);
+                    tr.find('td[class="history-address"]').html(json[i].address);
+                    tr.find('td[class="recognized-at-front"]').html(json[i].uploadDate);
+                    tbody.append(tr);
+                }
+                $('table-history-front').replaceWith(tbody);
+            },
+            showHistoryBack: function(json) {
+                var template = '<td class="history-file-back"><a data-toggle="modal" data-target="#img-box">IMAGE</a></td>' +
+                    '<td class="history-valid-period"></td>' +
+                    '<td class="history-released-by"></td>' +
+                    '<td class="recognised-at-back"></td>',
+                    tbody = $('<tbody></tbody>'),
+                    tr;
+                for(var i = 0, l= json.length; i < l; i++) {
+                    tr = $('<tr></tr>').html(template);
+                    tr.find('td[class="history-file-back"] a').attr('data-url', demo.config.apiPath + '/resources/' + json[i].source + '.jpg');
+                    tr.find('td[class="history-valid-period"]').html(json[i].validPeriod);
+                    tr.find('td[class="history-released-by"]').html(json[i].issueAuthority);
+                    tr.find('td[class="recognized-at-back"]').html(json[i].uploadDate);
+                    tbody.append(tr);
+                }
+                $('table-history-back').replaceWith(tbody);
+            },
+            showLoadingImg: function() {
+                $('#loading').show();
+            },
+            hideLoadingImg: function() {
+                $('#loading').hide();
+            }
         },
         controller: {
             init: function() {
@@ -28,18 +115,55 @@
                 $('#recognize-back').on('change', self.invokeRecognitionBack);
                 $('#a-history-front').on('shown.bs.tab', self.invokeHistoryDisplayFront);
                 $('#a-history-back').on('shown.bs.tab', self.invokeHistoryDisplayBack);
-                $('#sort-by-time').on('click', self.sortHistoryByTime());
             },
-            invokeRecognitionFront: function(e) {var file = e.target.files[0];},
-            handleRecognitionResultFront: function() {},
-            invokeRecognitionBack: function(e) {var file = e.target.files[0];},
-            handleRecognitionResultBack: function() {},
-            invokeHistoryDisplayFront: function(e) {},
-            handleHistoryDisplayFront: function() {},
-            invokeHistoryDisplayBack: function(e) {},
-            handleHistoryDisplayBack: function() {},
-            sortHistoryByTime: function() {},
-            history: {}
+            invokeRecognitionFront: function(e) {
+                demo.state.historyUpToDateFront = false;
+                demo.view.showLoadingImg();
+                var file = e.target.files[0];
+                demo.model.recognizeIdCard(file, 'front', this.handleRecognitionResultFront, this.ajaxErrorHandler);
+                demo.model.readAsUrl(file, demo.view.showUrlImgFront);
+            },
+            handleRecognitionResultFront: function(json) {
+                json = JSON.parse(json);
+                demo.view.showRecognitionResultFront(json);
+                demo.view.hideLoadingImg();
+            },
+            invokeRecognitionBack: function(e) {
+                demo.state.historyUpToDateBack = false;
+                demo.view.showLoadingImg();
+                var file = e.target.files[0];
+                demo.model.recognizeIdCard(file, 'back', this.handleRecognitionResultBack, this.ajaxErrorHandler);
+                demo.model.readAsUrl(file, demo.view.showUrlImgBack);
+            },
+            handleRecognitionResultBack: function(json) {
+                json = JSON.parse(json);
+                demo.view.showRecognitionResultBack(json);
+                demo.view.showLoadingImg();
+            },
+            ajaxErrorHandler: function(xmlHttpRequest) {
+                demo.view.hideLoadingImg();
+                console.log(xmlHttpRequest);
+            },
+            invokeHistoryDisplayFront: function() {
+                if(!demo.state.historyUpToDateFront) {
+                    demo.model.getHistoryFront(this.handleHistoryDisplayFront);
+                    demo.state.historyUpToDateFront = true;
+                }
+            },
+            handleHistoryDisplayFront: function(json) {
+                json = JSON.parse(json);
+                demo.view.showHistoryFront(json);
+            },
+            invokeHistoryDisplayBack: function() {
+                if(!demo.state.historyUpToDateBack) {
+                    demo.model.getHistoryBack(this.handleHistoryDisplayBack);
+                    demo.state.historyUpToDateBack = true;
+                }
+            },
+            handleHistoryDisplayBack: function(json) {
+                json = JSON.parse(json);
+                demo.view.showHistoryBack(json);
+            }
         },
         init: function() {
             this.controller.init();
